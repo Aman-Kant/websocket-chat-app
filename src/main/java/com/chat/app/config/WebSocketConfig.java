8b86ae1927b49fee6a866c4fd1f1dda65507c8f8
Aman Kant <eximiousezio@gmail.com>
2024-11-15 17:47:32 +0530

    cors policy updated

--- src/main/java/com/chat/app/config/WebSocketConfig.java
+++ src/main/java/com/chat/app/config/WebSocketConfig.java
@@ -14,7 +14,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         
         registry.addEndpoint("/chat")
-        .setAllowedOrigins("https://localhost:8080")
+        .setAllowedOrigins("*")
         .withSockJS();
     }
 
